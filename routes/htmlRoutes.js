f730cf4338938e82d55331b15a837c1f224f4714
Nadine Ju <nadine.jura@online.de>
2020-01-16 14:13:09 -0800

    style changed a bit

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,21 +3,21 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    // db.Example.findAll({}).then(function(dbExamples) {
+    //   res.render("index", {
+    //     msg: "Welcome!",
+    //     examples: dbExamples
+    //   });
+    // });
   });
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+    // db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+    //   res.render("example", {
+    //     example: dbExample
+    //   });
+    // });
   });
 
   // Render 404 page for any unmatched routes
