0eb0e543f988596e0eef880b2400cefc6c09975b
Tim Gangl <tim.gangl30@gmail.com>
2020-01-16 21:34:17 -0800

    more html built

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,18 +1,18 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    // db.Example.findAll({}).then(function(dbExamples) {
-    //   res.render("index", {
-    //     msg: "Welcome!",
-    //     examples: dbExamples
-    //   });
-    // });
+  app.get("/", function (req, res) {
+    db.Example.findAll({}).then(function (examplesdb) {
+      res.render("index", {
+        msg: "Welcome!",
+        database: examplesdb
+      });
+    });
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
+  app.get("/example/:id", function (req, res) {
     // db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
     //   res.render("example", {
     //     example: dbExample
@@ -21,7 +21,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
