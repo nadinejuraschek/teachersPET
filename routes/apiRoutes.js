87212ccfac180afe095d7a8e5f6d669a036a33b8
Jed Smit <jedsmit1@gmail.com>
2020-01-21 14:52:57 -0800

    write API routes for student and class

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -24,4 +24,45 @@ module.exports = function(app) {
       res.json(project2_db);
     });
   });
+
+  // Get all students
+  app.get("/api/students", function(req, res) {
+    db.Student.findAll({}).then(function(project2_db) {
+      res.json(project2_db);
+    });
+  });
+
+  // Get all students in a class
+  app.get("/api/students/classes/:id", function(req, res) {
+    db.Student.findAll({ where: { ClassId: req.params.id } }).then(function(
+      project2_db
+    ) {
+      res.json(project2_db);
+    });
+  });
+
+  // Create a new student
+  app.post("/api/students", function(req, res) {
+    db.Student.create(req.body).then(function(project2_db) {
+      res.json(project2_db);
+    });
+  });
+
+  // Update a student by id
+  app.put("/api/students/:id", function(req, res) {
+    db.Student.update({ where: { id: req.params.id } }).then(function(
+      project2_db
+    ) {
+      res.json(project2_db);
+    });
+  });
+
+  // Delete a student by id
+  app.delete("/api/students/:id", function(req, res) {
+    db.Student.destroy({ where: { id: req.params.id } }).then(function(
+      project2_db
+    ) {
+      res.json(project2_db);
+    });
+  });
 };
